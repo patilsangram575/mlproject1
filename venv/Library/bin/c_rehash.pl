0ee9d41993f1d33ecc6ef4a1046bf630a5eedfba
Sangram Patil <patilsangram575@gmail.com>
2023-06-01 20:48:18 +0530

    Initial commitintermediate

--- venv/Library/bin/c_rehash.pl
+++ venv/Library/bin/c_rehash.pl
@@ -13,7 +13,7 @@
 # and add symbolic links to their hash values.
 
 my $dir = "C:\\Program Files\\Common Files\\ssl";
-my $prefix = "C:/STUDY/ML-Project/Project1/venv\\Library";
+my $prefix = "c:/STUDY/ML-Project/Project1/venv\\Library";
 
 my $errorcount = 0;
 my $openssl = $ENV{OPENSSL} || "openssl";
